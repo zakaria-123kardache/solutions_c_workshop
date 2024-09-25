d41cff81afea24aaae3f86bd498695ae193148c5
unknown <zz2406143@gmail.com>
2024-09-25 19:53:53 +0100

    Conversion de la température

---
+++ Day_01/Les_Variables/Challeng_02.c
@@ -0,0 +1,15 @@
+ #include <stdio.h>
+
+int main()
+{
+    float C,K;
+    
+
+
+    printf("Donner la temperature C : \n");
+    scanf("%f",&C);
+    K=C+273.15;
+    printf("La reusultat est : %.2f",K);
+
+    return 0;
+}
