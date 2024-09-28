3e77fba6694e527791faea252c48839288ffa1d6
zakaria-123kardache <zz2406143@gmail.com>
2024-09-28 11:39:51 +0200

    Saisie et Affichage des Éléments

---
+++ Day_02/Challeng_02.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    int n ; 
+    int T[n] ,i; 
+
+
+   printf("affichage esst : %d", n);
+   scanf("%d",n); 
+
+
+
+   for ( i ; i < n ; i ++ ){
+   printf("affichage esst : %d",i ++);
+   scanf("%d",T[i]); 
+
+ }
+   printf("lafichage des elemnt estt : "); 
+
+   for ( i ; i < n ; i ++ ){
+   printf("affichage esst : %d",T[i]);
+   
+
+ }
