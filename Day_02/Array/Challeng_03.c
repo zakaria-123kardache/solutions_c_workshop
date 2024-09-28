1216ff0f4b3cda436a332ec1b41fa0a4ea314987
zakaria-123kardache <zz2406143@gmail.com>
2024-09-28 11:55:28 +0200

    Somme des Éléments

---
+++ Day_02/Array/Challeng_03.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    int n ; 
+    int T[n] ,i; 
+    int lasomme ; 
+
+
+   printf("affichage esst : %d", n);
+   scanf("%d",&n); 
+
+
+
+   for ( i ; i < n ; i ++ ){
+    
+   printf("affichage esst : %d",i ++);
+   scanf("%d",T[i]); 
+
+ }
+   lasomme = lasomme + T[i];
+
+   printf("lafichage la somme estt : "); 
+
+   for ( i ; i < n ; i ++ ){
+   printf("affichage esst : %d", lasomme);
+   
+
+ }
+
