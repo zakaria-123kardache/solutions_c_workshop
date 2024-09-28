7c916bdba0bba8b63c99c8cc6576a672490e682b
zakaria-123kardache <zz2406143@gmail.com>
2024-09-28 17:43:46 +0200

    Afficher les Éléments Impairs

---
+++ Day_02/Array/Challeng_13.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int n;
+    int i;
+
+    printf("Donneee nmbr  (n e N) : ");
+    scanf("%d",&n);
+
+    int T[n];
+
+    for(i=0;i<n;i++){
+
+        printf("entre la valeur %d : ",i+1);
+        scanf("%d",&T[i]);
+     }
+
+     for(i=0;i<n;i++){
+
+        if(T[i]%2 != 0){
+            printf("les valeur %d : \n",T[i]);
+        }
+     
+     }
+ 
+}
