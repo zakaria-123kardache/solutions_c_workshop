120c98b6ec4bbae26c4c98b46d5c2d8f03719194
unknown <zz2406143@gmail.com>
2024-09-29 16:50:10 +0100

    Fonction de Somme

---
+++ Day_02/Function/Challeneg_01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int zakaria(int x, int y) 
+{
+    return x + y ;
+}
+
+int main() {
+
+    int al_somme = zakaria(3, 7);
+
+    printf("alsomme esst %d\n", al_somme);  
+    
+    return 0;
+}
