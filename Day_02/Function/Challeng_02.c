8af344b382e8c713bde7c9e07bf11193932faf6f
unknown <zz2406143@gmail.com>
2024-09-29 16:54:27 +0100

    Fonction de Multiplication

---
+++ Day_02/Function/Challeng_02.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int zakaria(int x, int y) 
+{
+    return x * y ;
+}
+
+int main() {
+
+    int miltupl = zakaria(3, 7);
+
+    printf("la multiplication  entre deux nmbr esst  %d\n", miltupl);  
+    
+    return 0;
+}
